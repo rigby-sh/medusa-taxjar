e387fe05de44ca0866759c73df835c3379928e1a
Michal Slusarczyk <mslusarczyk@MacBook-Pro-Micha.local>
2024-11-28 10:00:16 +0100

    export constant

--- src/modules/taxcode/index.ts
+++ src/modules/taxcode/index.ts
@@ -1,7 +1,7 @@
 import { Module } from "@medusajs/framework/utils";
 import TaxCodeService from "./service";
 
-const TAX_CODE_SERVICE = "Taxcode";
+export const TAX_CODE_SERVICE = "Taxcode";
 
 export default Module(TAX_CODE_SERVICE, {
   service: TaxCodeService,
